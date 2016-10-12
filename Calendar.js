d5731bb5fe548b2e13223e335e0d642b70698899
Simon Reinsperger <reinsperger.smn@gmail.com>
2016-10-12 21:37:50 +0200

    Started splitting up functionality in several reusable functions Implements bluebird Promises

--- Calendar.js
+++ Calendar.js
@@ -1,13 +1,14 @@
 /*
-Authentication is heavily based on the quickstarter
-https://developers.google.com/google-apps/calendar/quickstart/nodejs
+ Authentication is heavily based on the quickstarter
+ https://developers.google.com/google-apps/calendar/quickstart/nodejs
  */
 
 const fs = require('fs'),
-      readline = require('readline'),
-      google = require('googleapis'),
-      googleAuth = require('google-auth-library'),
-      config = require('./config.json');
+  readline = require('readline'),
+  google = require('googleapis'),
+  googleAuth = require('google-auth-library'),
+  config = require('./config.json'),
+  Promise = require('bluebird');
 
 function Calendar () {
   this.TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) +
@@ -24,10 +25,10 @@ function Calendar () {
 Calendar.prototype.authorize = function (credentials, cb) {
 
   const clientSecret = credentials.installed.client_secret,
-        clientId = credentials.installed.client_id,
-        redirectUrl = credentials.installed.redirect_uris[0],
-        auth = new googleAuth(),
-        oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+    clientId = credentials.installed.client_id,
+    redirectUrl = credentials.installed.redirect_uris[0],
+    auth = new googleAuth(),
+    oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
   fs.readFile(this.TOKEN_PATH, (err, token) => {
     if (err) {
@@ -46,13 +47,13 @@ Calendar.prototype.authorize = function (credentials, cb) {
 Calendar.prototype.getNewToken = function (oauth2Client, cb) {
 
   const authUrl = oauth2Client.generateAuthUrl({
-          access_type: 'offline',
-          scope: this.SCOPES
-        }),
-        rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout
-        });
+      access_type: 'offline',
+      scope: this.SCOPES
+    }),
+    rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
   console.log('Authorize this app by visiting this url:', authUrl);
 
@@ -85,7 +86,56 @@ Calendar.prototype.storeToken = function (token) {
     }
     console.log('Token stored to ', this.TOKEN_PATH);
   });
+};
 
+Calendar.prototype.getCalendarList = function () {
+
+  const calendarAPI = google.calendar('v3'),
+    authPromise = this.getAuth();
+
+  return new Promise(function (resolve, reject) {
+    authPromise
+      .then(function (auth) {
+        calendarAPI.calendarList.list({
+          auth: auth
+        }, (err, response) => {
+
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve(response);
+        });
+
+      })
+      .catch(function (err) {
+        return reject(err);
+      });
+  });
+};
+
+Calendar.prototype.getAuth = function () {
+
+  const that = this;
+
+  return new Promise(function (resolve, reject) {
+    if (that.auth) {
+      resolve(that.auth);
+    } else {
+      fs.readFile('client_secret.json', (err, content) => {
+        if (err) return reject(err);
+
+        try {
+          that.authorize(JSON.parse(content), function (auth) {
+            that.auth = auth;
+            resolve(that.auth);
+          });
+        } catch (e) {
+          return reject(e);
+        }
+      });
+    }
+  });
 };
 
 module.exports = Calendar;
@@ -95,58 +145,66 @@ module.exports = Calendar;
 
   const cal = new Calendar();
 
-  fs.readFile('client_secret.json', (err, content) => {
-    if (err) {
-      console.log('Error loading client secret file:', err);
-      return;
-    }
+  cal.getCalendarList()
+    .then( calendarList => {
+      console.log('calendarList', calendarList);
+    })
+    .catch( err => {
+      console.log('Error', err)
+    });
 
-    try {
-      cal.authorize(JSON.parse(content), function (auth) {
-        var calendar = google.calendar('v3');
-
-        calendar.calendarList.list({
-          auth: auth
-        }, (err, response) => {
-          if (err) {
-            console.log('Error while getting calendar list:', err);
-            return;
-          }
-
-          let schedule = response.items.filter(c => c.summary === config.username)[0];
-          console.log(config.username, schedule);
-
-          if ( ! schedule ) {
-            console.log('Create new calendar');
-
-            calendar.calendars.insert({
-              auth: auth,
-              resource: {
-                summary: config.username,
-                timeZone: 'Europe/Vienna'
-              }
-            }, (err, newCalendar) => {
-
-              if (err) {
-                console.log('Error while creating new calendar:', err);
-                return;
-              }
-
-              console.log('new calendar:', newCalendar);
-
-            });
-
-          } else {
-            console.log('Calendar already exists');
-          }
-
-        });
-
-      });
-    } catch (err) {
-      console.log('Error parsing credentials:', err);
-
-    }
-
-  });
+  // fs.readFile('client_secret.json', (err, content) => {
+  //   if (err) {
+  //     console.log('Error loading client secret file:', err);
+  //     return;
+  //   }
+  //
+  //   try {
+  //     cal.authorize(JSON.parse(content), function (auth) {
+  //       var calendar = google.calendar('v3');
+  //
+  //       calendar.calendarList.list({
+  //         auth: auth
+  //       }, (err, response) => {
+  //         if (err) {
+  //           console.log('Error while getting calendar list:', err);
+  //           return;
+  //         }
+  //
+  //         let schedule = response.items.filter(c => c.summary === config.username)[0];
+  //         console.log(config.username, schedule);
+  //
+  //         if ( ! schedule ) {
+  //           console.log('Create new calendar');
+  //
+  //           calendar.calendars.insert({
+  //             auth: auth,
+  //             resource: {
+  //               summary: config.username,
+  //               timeZone: 'Europe/Vienna'
+  //             }
+  //           }, (err, newCalendar) => {
+  //
+  //             if (err) {
+  //               console.log('Error while creating new calendar:', err);
+  //               return;
+  //             }
+  //
+  //             console.log('new calendar:', newCalendar);
+  //
+  //           });
+  //
+  //         } else {
+  //           console.log('Calendar already exists');
+  //         }
+  //
+  //       });
+  //
+  //     });
+  //   } catch (err) {
+  //     console.log('Error parsing credentials:', err);
+  //
+  //   }
+  //
+  // });
 })();
